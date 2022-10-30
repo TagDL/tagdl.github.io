76f560e36d80eae807cd751cb07801ed7de8bbea
TagDL <69299863+TagDL@users.noreply.github.com>
2022-10-30 22:04:10 +0800

    Delete js directory

--- js/otherani.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $(".container").each(function (i) {
-        $(this).css( {'opacity': 0, 'bottom': '-100px' } )
-    .animate( { 'opacity': '1', 'bottom' : 0 }, 1000 + i*500);
-    })
-})
